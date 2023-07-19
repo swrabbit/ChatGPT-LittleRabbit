5a9440a8ed9febc985254bdde83a31c21581ae1f
swrabbit Aley <rabbit2015@163.com>
2023-07-19 11:42:03 +0000

    modified:   app/components/sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT & LRabbit
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          your own AI assistant.
+          Your own AI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
