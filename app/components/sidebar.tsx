da2e8a4e7aec710dc58504a0c183be177ddb0c5d
swrabbit Aley <rabbit2015@163.com>
2024-01-02 12:42:40 +0000

    modified:   app/components/sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           NextChat
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          你的专属 AI 智能小助手
+          Build your own AI assistant.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
