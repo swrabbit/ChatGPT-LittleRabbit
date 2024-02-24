8f0841b70a3e1215619ea18a596de1fe426a2a9c
swrabbit Aley <rabbit2015@163.com>
2024-02-24 06:21:11 +0000

    modified:   app/components/settings.tsx

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -700,7 +700,7 @@ export function Settings() {
             </Popover>
           </ListItem>
 
-          {/* <ListItem // 设置里不显示检查版本 Supaleyer
+          <ListItem // 设置里不显示检查版本 Supaleyer
             title={Locale.Settings.Update.Version(currentVersion ?? "unknown")}
             subTitle={
               checkingUpdate
@@ -723,7 +723,7 @@ export function Settings() {
                 onClick={() => checkUpdate(true)}
               />
             )}
-          </ListItem> */}
+          </ListItem>
 
           <ListItem title={Locale.Settings.SendKey}>
             <Select
